d1429947a9270be15574c1531e89b1fc79b8780b
Ignacio Bona <igbopie@gmail.com>
2014-07-08 12:59:53 +0000

    run command

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID=AKIAJ4GCCVHYQANYFALA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_S3_BUCKET=left-dev-test-local
+nodejs app.js
