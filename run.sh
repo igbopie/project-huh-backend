431da6be6effb5a518465b77c6e38b16ceb69b5a
Ignacio Bona <ignacio.contractor@vnera.com>
2014-11-04 20:16:15 -0800

    Setup.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export AWS_ACCESS_KEY_ID=AKIAJ4GCCVHYQANYFALA
 export AWS_SECRET_ACCESS_KEY=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
 export AWS_S3_BUCKET=left-dev-test-local
-nodejs app.js
+node app.js
