0cf531484ba2be848093d662f3e2474f2e65b709
Ignacio Bona <ibona@vnera.com>
2015-07-26 22:45:57 -0700

    force ssl

--- src-frontend/js/services/httpsService.js
+++ src-frontend/js/services/httpsService.js
@@ -8,7 +8,7 @@ services
     .service('HttpsService', function ($location) {
         return {
             force: function () {
-                if ($location.protocol() !== 'https' && !_.startsWith($location.absUrl(), "http://localhost")) {
+                if ($location.protocol() !== 'https' && !_.startsWith($location.absUrl(), 'http://localhost')) {
                     window.location.href = $location.absUrl().replace(/^http/, 'https');
                 }
             }
